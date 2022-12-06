eb4586fad7361dc97ca3d4d7f32a4e8d2edb580a
github-io-bhar <86400328+github-io-bhar@users.noreply.github.com>
2022-12-06 20:45:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var rightWristX = 0;
 var rightWristY = 0;
 var harry_potter = "";
 var peter_pan = "";
+var scoreLeftWrist = 0;
+var song_status = "false";
 function preload() {
     harry_potter = loadSound("music.mp3");
     peter_pan = loadSound("music2.mp3");
@@ -21,10 +23,23 @@ function modelLoaded() {
 }
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX, leftWristY, 20);
+        peter_pan.stop();
+        if(song_status = "false"){
+            harry_potter.play();
+            document.getElementById("song_name").innerHTML = " Song Currently Playing: Harry Potter ";
+            song_status = "true";
+        }
+    }
 }
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("Score of Left Wrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("Left Wrist X = " + leftWristX + " Left Wrist Y = " + leftWristY);
